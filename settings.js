7f4ec4b1321d70e9c02f1a0278dc11cfe8be1cbd
shenbro918 <108683220+shenbro918@users.noreply.github.com>
2022-07-06 17:47:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,14 +32,14 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
+global.owner = ['94772830536'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['94772830536'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94772830536'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'Nishen' // Your name ඔබේ නම 
+global.botnma = '𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 ' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = '𝐍𝐢𝐬𝐡𝐞𝐧' //ownernama,ownername
 global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.author = '𝐧𝐢𝐬𝐡𝐞𝐧' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
